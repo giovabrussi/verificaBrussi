12071d4e9ce4128968d801535e5588af16ca7400
brussi <brussi.giovanni@itismeucci.com>
2023-11-15 13:26:32 +0100

    modifiche client

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -10,59 +10,56 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        try { 
-            System.out.println( "Il client è partito" );
+public class App {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Il client è partito");
             Socket s = new Socket("localhost", 3000);
 
             Scanner sin = new Scanner(System.in);
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-            System.out.println( "Connessione effettuata, premere 0 per uscire" );
-            System.out.println( "Digita una lettera per vedere se c'è oppure inserisci la parola per indovinare" );
+            System.out.println("Connessione effettuata, premere 0 per uscire");
+            System.out.println("Digita una lettera per vedere se c'è oppure inserisci la parola per indovinare");
 
             int risposta = 0;
 
             do {
 
                 String parola = in.readLine();
-                System.out.println( "Parola da indovinare: " + parola);
+                System.out.println("Parola da indovinare: " + parola);
 
                 String n = sin.nextLine();
-                out.writeBytes(n+"\n");
+                out.writeBytes(n + "\n");
 
                 String confronto = in.readLine();
                 risposta = Integer.parseInt(confronto);
-                
-                if(risposta == 1){
-                    System.out.println( "La lettera non è nella parola" );
-                }
-                if(risposta == 2){
-                    System.out.println( "La lettera è nella parola" );
-                }
-                if(risposta == 3){
-                    String c = in.readLine();
-                    int r = Integer.parseInt(c);
-                    System.out.println( "La parola non è quella giusta, hai perso dopo "+ r +" tentativi" );
-                    s.close();
-                }
-                if(risposta == 4){
-                    String c = in.readLine();
-                    int r = Integer.parseInt(c);
-                    System.out.println( "Hai indovinato la parola in "+ r +" tentativi, complimenti" );
-                    s.close();
-                }
-                if(risposta == 5){
-                    System.out.println( "Uscita" );
-                    s.close();
-                }
-            } while (risposta < 3);
 
-            
+                switch (risposta) {
+                    case 1:
+                        System.out.println("La lettera non è nella parola");
+                        break;
+                    case 2:
+                        System.out.println("La lettera è nella parola");
+                        break;
+                    case 3:
+                        System.out.println("La parola non è quella giusta, hai perso dopo "
+                                + Integer.parseInt(in.readLine()) + " tentativi");
+                        s.close();
+                        break;
+                    case 4:
+                        System.out.println("Hai indovinato la parola in " + Integer.parseInt(in.readLine())
+                                + " tentativi, complimenti");
+                        s.close();
+                        break;
+                    case 5:
+                        System.out.println("Uscita");
+                        s.close();
+                        break;
+                }
+
+            } while (risposta < 3);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- server/src/main/java/com/example/MioThread.java
+++ server/src/main/java/com/example/MioThread.java
@@ -7,24 +7,24 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MioThread extends Thread{
+public class MioThread extends Thread {
 
     Socket s;
 
-    public MioThread(Socket s){
+    public MioThread(Socket s) {
         this.s = s;
     }
 
-    public void run(){
+    public void run() {
         try {
 
             System.out.println("Un client si è collegato");
 
-            String[] parole = {"gondole", "dvd", "francese", "vergogna", "lago", "originale"};
+            String[] parole = { "gondole", "dvd", "francese", "vergogna", "lago", "originale" };
             ArrayList<String> caratteri = new ArrayList<>();
 
             Random random = new Random();
-            String parola = parole[random.nextInt(6)+1];
+            String parola = parole[random.nextInt(6) + 1];
             String sout = "";
 
             for (int i = 0; i < parola.length(); i++) {
@@ -35,70 +35,72 @@ public class MioThread extends Thread{
 
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            
+
             int risposta = 0;
             int c = 0;
 
             do {
-                out.writeBytes(sout+"\n");
+                out.writeBytes(sout + "\n");
 
                 String stringaRicevuta = in.readLine();
                 c++;
-                if(stringaRicevuta.equals("0")){
+
+                if (stringaRicevuta.equals("0")) {
                     out.writeBytes("5\n");
                     risposta = 5;
-                }else if(stringaRicevuta.length() == 1){
+                } else if (stringaRicevuta.length() == 1) {
 
-                    if(parola.indexOf(stringaRicevuta) < 0){
+                    if (parola.indexOf(stringaRicevuta) < 0) {
 
                         out.writeBytes("1\n");
                         risposta = 1;
 
-                    }else{
+                    } else {
 
                         caratteri.add(stringaRicevuta);
                         sout = "";
 
                         for (int i = 0; i < parola.length(); i++) {
 
-                            String p = String.valueOf(parola.charAt(i)) ;
-                            if(caratteri.contains(p)){
+                            String p = String.valueOf(parola.charAt(i));
+                            if (caratteri.contains(p)) {
                                 sout += p;
-                            }else{
+                            } else {
                                 sout += "*";
                             }
 
                         }
 
+                        if (sout.contains("*")) {
+                            out.writeBytes("2\n");
+                            risposta = 2;
+                        } else {
+                            out.writeBytes("4\n");
+                            out.writeBytes(c + "\n");
+                            risposta = 4;
+                        }
 
-                        out.writeBytes("2\n");
-                        risposta = 2; 
                     }
 
-                }else{
+                } else {
 
-
-                    if(!stringaRicevuta.equals(parola)){
+                    if (!stringaRicevuta.equals(parola)) {
                         out.writeBytes("3\n");
-                        out.writeBytes(c+"\n");
-                        risposta = 3; 
-                    }else{
+                        out.writeBytes(c + "\n");
+                        risposta = 3;
+                    } else {
                         out.writeBytes("4\n");
-                        out.writeBytes(c+"\n");
+                        out.writeBytes(c + "\n");
 
                         risposta = 4;
                     }
 
                 }
-                
+
             } while (risposta < 3);
 
-            
-
-
-
             s.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("errore durante l'istanza del server");
             System.exit(1);
